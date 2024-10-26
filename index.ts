2741e1fa36e7ee06fbfc098c32c7f9632743d106
aauroraaborealisrs <imaginepeach@mail.ru>
2024-10-26 23:36:34 +0300

    feat: add server

---
+++ index.ts
@@ -0,0 +1,39 @@
+import { WebSocketServer } from "ws";
+import { httpServer } from "./src/http_server";
+
+const HTTP_PORT = 3000;
+export const wss = new WebSocketServer({ server: httpServer });
+
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+
+  ws.on("message", (data) => {
+    try {
+      const message = JSON.parse(data.toString());
+      console.log("Message received:", message);
+    } catch (error) {
+      console.error("Message parsing error:", error);
+      ws.send(JSON.stringify({ error: "Invalid message format" }));
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+export const terminateServer = () => {
+  console.log("Initiating server termination...");
+  wss.clients.forEach((client) => client.readyState === client.OPEN && client.close());
+  wss.close(() => {
+    console.log("WebSocket server successfully terminated.");
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", terminateServer);
+process.on("SIGTERM", terminateServer);
+
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Server is running at http://localhost:${HTTP_PORT}`);
+});
